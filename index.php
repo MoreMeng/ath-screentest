219feb4c077a3c93355db88ef3d7ca33f55ca8f2
wasuwe <40377567+wasuwe@users.noreply.github.com>
2018-08-14 15:47:41 +0700

    V1.0

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Monitor Calibration Graphic</title>
+    <title>Monitor Calibration Graphic V1.0</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootswatch/3.3.7/flatly/bootstrap.min.css" rel="stylesheet" integrity="sha384-+ENW/yibaokMnme+vBLnHMphUYxHs34h9lpdbSLuAwGkOKFRl4C34WkjazBtb7eT"
