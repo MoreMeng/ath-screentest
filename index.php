00fa394407d855b514bdab633ee783c83ea79829
Thanikul Sriuthis <moremeng@dv4.biz>
2018-08-29 21:00:37 +0700

    เปลี่ยน รูปแบบการทำงานใหม่ เพิ่ม style จาก ath-starter

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
+<?php
+session_start();
+// error_reporting(0);
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+
+require realpath('../dv-config.php');
+require DEV_PATH . '/classes/db.class.v2.php';
+require DEV_PATH . '/functions/global.php';
+
+
+// define("starttime", getmicrotime());
+$GET_DEV = (empty($_GET['dev'])) ? null : $_GET['dev'];
+// $GET_NUMBER = (isset($_GET['number'])) ? $_GET['number'] : '';
+// $GET_MODE = (isset($_GET['mode'])) ? $_GET['mode'] : null;
+// $GET_ID = (isset($_GET['id'])) ? $_GET['id'] : null;
+
+define('Q_VERSION', '1.0.0');
+
+$dept = CON::selectArrayDB(array(),"SELECT sec_id,sec_code,sec_name FROM ath_department_section ORDER BY sec_name");
+?>
+
 <!DOCTYPE html>
 <html lang="">
 
@@ -6,128 +27,76 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Monitor Calibration Graphic</title>
-
-    <!-- Bootstrap CSS -->
-    <link href="https://stackpath.bootstrapcdn.com/bootswatch/3.3.7/flatly/bootstrap.min.css" rel="stylesheet" integrity="sha384-+ENW/yibaokMnme+vBLnHMphUYxHs34h9lpdbSLuAwGkOKFRl4C34WkjazBtb7eT"
-        crossorigin="anonymous">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/css/select2.min.css" rel="stylesheet" />
-    <style>
-        #q1 {
-            width: 100%;
-            height: 450px;
-            background: rgb(0, 0, 0);
-            background: radial-gradient(circle, rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 1) 5%, rgba(13, 13, 13, 1) 5%, rgba(13, 13, 13, 1) 10%, rgba(26, 26, 26, 1) 10%, rgba(26, 26, 26, 1) 20%, rgba(38, 38, 38, 1) 20%, rgba(38, 38, 38, 1) 30%, rgba(64, 64, 64, 1) 30%, rgba(64, 64, 64, 1) 100%);
-        }
-        #q2 {
-            width: 100%;
-            height: 450px;
-            background: rgb(255, 255, 255);
-            background: radial-gradient(circle, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 1) 5%, rgba(242, 242, 242, 1) 5%, rgba(242, 242, 242, 1) 10%, rgba(230, 230, 230, 1) 10%, rgba(230, 230, 230, 1) 20%, rgba(217, 217, 217, 1) 20%, rgba(217, 217, 217, 1) 30%, rgba(230, 230, 230, 1) 30%, rgba(220, 220, 220, 1) 100%);
-        }
-        #q3 {
-            width: 100%;
-            height: 450px;
-            background: rgb(0, 0, 0);
-            background: linear-gradient(90deg, rgba(0, 0, 0, 1) 0%, rgba(255, 255, 255, 1) 100%);
-        }
-        #q4 {
-            width: 100%;
-            height: 450px;
-            background: rgb(0, 0, 0);
-            background: linear-gradient(90deg,
-            rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 1) 4.76%, rgba(13, 13, 13, 1) 4.76%, rgba(13, 13, 13, 1) 9.52%, rgba(26, 26, 26, 1) 9.52%, rgba(26, 26, 26, 1) 14.29%, rgba(38, 38, 38, 1) 14.29%, rgba(38, 38, 38, 1) 19.05%, rgba(51, 51, 51, 1) 19.05%, rgba(51, 51, 51, 1) 23.81%, rgba(64, 64, 64, 1) 23.81%, rgba(64, 64, 64, 1) 28.57%, rgba(77, 77, 77, 1) 28.57%, rgba(77, 77, 77, 1) 33.33%, rgba(89, 89, 89, 1) 33.33%, rgba(89, 89, 89, 1) 38.10%, rgba(102, 102, 102, 1) 38.10%, rgba(102, 102, 102, 1) 42.86%, rgba(115, 115, 115, 1) 42.86%, rgba(115, 115, 115, 1) 47.62%, rgba(128, 128, 128, 1) 47.62%, rgba(128, 128, 128, 1) 52.38%, rgba(140, 140, 140, 1) 52.38%, rgba(140, 140, 140, 1) 57.14%, rgba(153, 153, 153, 1) 57.14%, rgba(153, 153, 153, 1) 61.90%, rgba(166, 166, 166, 1) 61.90%, rgba(166, 166, 166, 1) 66.67%, rgba(179, 179, 179, 1) 66.67%, rgba(179, 179, 179, 1) 71.43%, rgba(191, 191, 191, 1) 71.43%, rgba(191, 191, 191, 1) 76.19%, rgba(204, 204, 204, 1) 76.19%, rgba(204, 204, 204, 1) 80.95%, rgba(217, 217, 217, 1) 80.95%, rgba(217, 217, 217, 1) 85.71%, rgba(230, 230, 230, 1) 85.71%, rgba(230, 230, 230, 1) 90.48%, rgba(242, 242, 242, 1) 90.48%, rgba(242, 242, 242, 1) 95.24%, rgba(255, 255, 255, 1) 95.24%, rgba(255, 255, 255, 1) 100.00%);
-        }
-        #quiz {
-            text-align: center;
-            display: none;
-        }
-        #prev,
-        #start {
-            display: none;
-        }
-        .stick {
-            position: absolute;
-            left: 1%;
-            top: 0;
-        }
-        label.btn {
-            padding: 8px 40px;
-        }
-        th{
-            text-align:center;
-        }
-    </style>
+    <!-- CORE CSS -->
+    <link rel="stylesheet" href="<?php echo ASSET_PATH; ?>/bootswatch/flatly/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo ASSET_PATH; ?>/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="<?php echo ASSET_PATH; ?>/fonts/maledpan/maledpan.css">
+    <link rel="stylesheet" href="<?php echo ASSET_PATH; ?>/fonts/chatthai/chatthai.css">
+    <link rel="stylesheet" href="<?php echo ASSET_PATH; ?>/sweetalert-lastest/dist/sweetalert.css">
+    <link rel="stylesheet" href="<?php echo ASSET_PATH; ?>/select2/dist/css/select2.css">
+    <link rel="stylesheet" href="<?php echo ASSET_PATH; ?>/select2-bootstrap-theme/dist/select2-bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo SITE_URL; ?>/css/screentest.css?v=<?php echo filemtime('css/screentest.css'); ?>">
+    <?php if (!in_array($GET_DEV, array('','view','report'))) : ?>
+    <link rel="stylesheet" href="<?php echo ASSET_PATH; ?>/datatables/media/css/dataTables.bootstrap.min.css">
+    <?php elseif ($GET_DEV == 'report') : ?>
+    <!-- <link rel="stylesheet" href="<?php echo ASSET_PATH; ?>/morris.js/morris.css"> -->
+    <?php endif; ?>
 </head>
+
 <body>
-    <?php include 'connect.php'; ?>
     <div id="wrapper">
         <div id='quiz'></div>
-        
+
         <nav>
             <ul class="pager">
-                <li>
-                    <a id="prev" href='#'>Prev</a>
-                </li>
-                <div id="send" class="container">
-                        <table class="table table-hover" >
-                            <tr>
-                                <th>Name : </th><td><input class="form-control"  type="text" id="namee" name="namee" autocomplete="off"></td>
-                            </tr>
-                            <tr>
-                                <th>Monitor :</th><td><input class="form-control"  type="text" id="monitor" name="monitor" autocomplete="off"></td>
-                            </tr>
-                            <tr>
-                                <th>Section :</th><td>
-                                        <select class="js-example-basic-single" name="section" id="section" style="width: 100%" >
-                                                <?php
-                                                    $sql = "SELECT sec_id,sec_code,sec_name FROM ath_department_section";
-                                                    $result3=mysqli_query($conn,$sql);
-                                        
-                                                    if ($result3->num_rows > 0) {
-                                                        
-                                                        while($row1 = $result3->fetch_assoc()) {
-                                                            echo " <option value='".$row1['sec_id']."'>".$row1['sec_code']." - ".$row1['sec_name']."</option>";
-                                            
-                                                        }
-                                                    } else {
-                                                        echo "0 results";
-                                                    }   
-                                                ?>
-                                                </select>
-                                </td>
-                            </tr>
-                        </table>
-                        <ul class="pager">
-                                    <li>
-                                        <a id="Enter"  href='#'>Send</a>
-                                    </li>
-                                    <li>
-                                        <a id="start" href='#'>Start Over</a>
-                                    </li>
-                            </ul>
-                    </div>
-                    <div id="sent">
-                        <h2>ส่งข้อมูลแล้ว</h2>
-                    </div>
-                <li>
-                    <a id="next" href='#'>Next</a>
-                </li>
+                <li> <a id="prev" href='#'>Prev</a> </li>
+                <li> <a id="next" href='#'>Next</a> </li>
             </ul>
         </nav>
-    </div>
-    <!-- jQuery -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.1/js/select2.full.min.js"></script>
-        <script>
-        $(document).ready(function() {
-            $(".js-example-basic-single").select2();
-        });
-        </script>
 
-    <!-- Bootstrap JavaScript -->
+        <div id="send" class="container-fluid">
+            <div class="col-xs-4 col-xs-offset-4 well well-lg">
+
+                <form action="" method="POST" class="form-horizontal" role="form">
+                    <div class="form-group">
+                        <label for="name">Name:</label>
+                        <input class="form-control" type="text" id="name" name="name" placeholder="ชื่อ-นามสกุล ผู้ทำการทดสอบ">
+                    </div>
+                    <div class="form-group">
+                        <label for="monitor">Monitor:</label>
+                        <input class="form-control" type="text" id="monitor" name="monitor" placeholder="ยี่ห้อ รุ่น ของหน้าจอ ตัวอย่าง: Acer X193HQ A">
+                    </div>
+                    <div class="form-group">
+                        <label for="section">Section:</label>
+                        <select class="form-control select2" name="section" id="section">
+                            <?php echo dropdown( $dept, null, 'sec_id', 'sec_name'); ?>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <div class="text-center">
+                            <button id="Enter" type="button" class="btn btn-primary">บันทึกผลการทดสอบ</button>
+                        </div>
+                    </div>
+                </form>
+                <!-- <ul class="pager">
+                <li><a id="start" href='#'>Start Over</a></li>
+            </ul> -->
+            </div>
+        </div>
+        <div id="sent">
+            <h2>ส่งข้อมูลแล้ว</h2>
+        </div>
+    </div>
+
+    <script src="<?php echo ASSET_PATH; ?>/jquery/dist/jquery.min.js"></script>
+    <script src="<?php echo ASSET_PATH; ?>/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="<?php echo ASSET_PATH; ?>/select2/dist/js/select2.min.js"></script>
+    <script src="<?php echo ASSET_PATH; ?>/sweetalert2/dist/sweetalert.min.js"></script>
+
+
     <script>
-        (function () {
+        $(function () {
+            'use strict';
             const questions = [{
                 question: "เห็นวงกลมในรูปกี่วง",
                 choices: ["3 วง", "4 วง"],
@@ -160,7 +129,7 @@
                 choose();
                 // If no user selection, progress is stopped
                 if (isNaN(selections[questionCounter])) {
-                    alert('Please make a selection!');
+                    swal("อ๊ะ!", "กรุณาเลือกคำตอบก่อนนะครับ", "warning",{button: "ปิด", dangerMode: true});
                 } else {
                     questionCounter++;
                     displayNext();
@@ -195,26 +164,26 @@
                         numCorrect++;
                     }
                 }
-                
-                
-                $.ajax({
-                url: "insert.php?score="+numCorrect+"&name="+namee.value+"&monitor="+monitor.value+"&section="+section.value,
-                type: 'post',
-                dataType: 'html'
-                });
-                $('#send').hide();
-                $('#quiz').hide();
-                $('#start').hide();
-                
 
-                $('#start').hide();
+
+                $.ajax({
+                    url: "insert.php?score=" + numCorrect + "&name=" + name.value + "&monitor=" +
+                        monitor.value + "&section=" + section.value,
+                    type: 'post',
+                    dataType: 'html'
+                });
+                $('#send,#quiz,#start,#start').hide();
+
                 $('#sent').show();
 
-                setTimeout(window.open("http://ath4/","_self"),3000);
-                
-                
+                setTimeout(window.open("http://ath4/", "_self"), 3000);
+
+
             });
 
+            $('.select2').select2({
+                theme: 'bootstrap'
+            });
             // Creates and returns the div that contains the questions and
             // the answer selections
             function createQuestionElement(index) {
@@ -291,11 +260,12 @@
                         numCorrect++;
                     }
                 }
-                score.append('You got ' + numCorrect + ' questions out of ' +
-                    questions.length + ' right!!!');
+                // score.append('You got ' + numCorrect + ' questions out of ' + questions.length + ' right!!!');
+                score.append('ขอบคุณสำหรับการทำแบบทดสอบ');
                 return score;
             }
-        })();
+        });
     </script>
 </body>
+
 </html>
