6d69d2f73eb2082d16512ccaf693152012a70030
Thanikul Sriuthis <moremeng@dv4.biz>
2018-09-14 17:04:26 +0700

    version 1.0.0

--- index.php
+++ index.php
@@ -1,22 +1,16 @@
 <?php
 session_start();
-// error_reporting(0);
-error_reporting(E_ERROR | E_WARNING | E_PARSE);
+error_reporting(0);
+// error_reporting(E_ERROR | E_WARNING | E_PARSE);
 
 require realpath('../dv-config.php');
 require DEV_PATH . '/classes/db.class.v2.php';
 require DEV_PATH . '/functions/global.php';
 
-
-// define("starttime", getmicrotime());
 $GET_DEV = (empty($_GET['dev'])) ? null : $_GET['dev'];
-// $GET_NUMBER = (isset($_GET['number'])) ? $_GET['number'] : '';
-// $GET_MODE = (isset($_GET['mode'])) ? $_GET['mode'] : null;
-// $GET_ID = (isset($_GET['id'])) ? $_GET['id'] : null;
 
 define('Q_VERSION', '1.0.0');
 
-$dept = CON::selectArrayDB(array(),"SELECT sec_id,sec_code,sec_name FROM ath_department_section ORDER BY sec_name");
 ?>
 
 <!DOCTYPE html>
@@ -44,13 +38,14 @@ $dept = CON::selectArrayDB(array(),"SELECT sec_id,sec_code,sec_name FROM ath_dep
 </head>
 
 <body>
+<div id="bg"></div>
     <div id="wrapper">
         <div id='quiz'></div>
 
         <nav>
             <ul class="pager">
-                <li> <a id="prev" href='#'>Prev</a> </li>
-                <li> <a id="next" href='#'>Next</a> </li>
+                <li> <a id="prev" href='#'>ก่อนหน้า</a> </li>
+                <li> <a id="next" href='#'>ถัดไป</a> </li>
             </ul>
         </nav>
 
@@ -68,204 +63,66 @@ $dept = CON::selectArrayDB(array(),"SELECT sec_id,sec_code,sec_name FROM ath_dep
                     </div>
                     <div class="form-group">
                         <label for="section">Section:</label>
-                        <select class="form-control select2" name="section" id="section">
-                            <?php echo dropdown( $dept, null, 'sec_id', 'sec_name'); ?>
+                        <select data-placeholder="เลือกหน่วยงาน" class="form-control select2" name="section" id="section">
+                        <option></option>
+                                <?php
+                            // foreach ($da_section as $row) {
+                            //  $select = ($row['sec_id'] == $com[0]['ser_section']) ? 'selected' : '';
+                            //  echo '<option value="'.$row['sec_id'].'" '.$select .'>'.pad($row['sec_code']).$row['sec_name'].'</option>'.PHP_EOL;
+                            // }
+                            $sql_section = "SELECT
+                            " . TB_PREFIX . "department_section.sec_id,
+                            " . TB_PREFIX . "department_section.sec_name,
+                            " . TB_PREFIX . "department_section.sec_code,
+                            " . TB_PREFIX . "department.dep_id,
+                            " . TB_PREFIX . "department.dep_name
+                            FROM
+                            " . TB_PREFIX . "department_section
+                            INNER JOIN " . TB_PREFIX . "department ON " . TB_PREFIX . "department_section.dept_id = " . TB_PREFIX . "department.dep_id
+                            ORDER BY
+                            " . TB_PREFIX . "department_section.dept_id ASC,
+                            " . TB_PREFIX . "department_section.sec_name ASC";
+
+                            $section = CON::selectArrayDB(array(), $sql_section);
+                            $selected = $def = '';
+                            foreach ($section as $row) {
+                                // $select = ($selected==$row['sec_id']) ? ' selected' : null;
+                                // $select = ($row['sec_id'] == $com[0]['ser_section']) ? 'selected' : '';
+                                $select = ($row['sec_id'] == $_SESSION['ses_sec_id']) ? 'selected' : '';
+
+                                if ($row['dep_id'] != $def) {
+                                    echo ($def != '') ? '</optgroup>' : '';
+                                    echo '<optgroup label="' . $row['dep_name'] . '">';
+                                    $def = $row['dep_id'];
+                                    echo '<option value="' . $row['sec_id'] . '" ' . $select . '>' . $row['sec_code'] . ' - ' . $row['sec_name'] . '</option>';
+                                } else {
+                                    echo '<option value="' . $row['sec_id'] . '" ' . $select . '>' . $row['sec_code'] . ' - ' . $row['sec_name'] . '</option>';
+                                }
+                            }
+                            echo '</optgroup>';
+                            ?>
                         </select>
                     </div>
                     <div class="form-group">
                         <div class="text-center">
-                            <button id="Enter" type="button" class="btn btn-primary">บันทึกผลการทดสอบ</button>
+                            <button id="submit" type="button" class="btn btn-primary">บันทึกผลการทดสอบ</button>
                         </div>
                     </div>
                 </form>
-                <!-- <ul class="pager">
-                <li><a id="start" href='#'>Start Over</a></li>
-            </ul> -->
+
             </div>
-        </div>
-        <div id="sent">
-            <h2>ส่งข้อมูลแล้ว</h2>
         </div>
     </div>
 
+    <?php require 'footer.php'; ?>
+
+    <script src="<?php echo SITE_URL; ?>/js/loadbg.js" async></script>
     <script src="<?php echo ASSET_PATH; ?>/jquery/dist/jquery.min.js"></script>
     <script src="<?php echo ASSET_PATH; ?>/bootstrap/dist/js/bootstrap.min.js"></script>
     <script src="<?php echo ASSET_PATH; ?>/select2/dist/js/select2.min.js"></script>
     <script src="<?php echo ASSET_PATH; ?>/sweetalert2/dist/sweetalert.min.js"></script>
+    <script src="<?php echo SITE_URL; ?>/js/app.js"></script>
 
-
-    <script>
-        $(function () {
-            'use strict';
-            const questions = [{
-                question: "เห็นวงกลมในรูปกี่วง",
-                choices: ["3 วง", "4 วง"],
-                correctAnswer: 1
-            }, {
-                question: "เห็นวงกลมในรูปกี่วง",
-                choices: ["3 วง", "4 วง"],
-                correctAnswer: 1
-            }, {
-                question: "เฉดสีเรียงกันสม่ำเสมอหรือไม่",
-                choices: ["ใช่", "ไม่ใช่"],
-                correctAnswer: 0
-            }, {
-                question: "มีแท่งสีจำนวนกี่แท่ง เฉดสีดำ-ขาว",
-                choices: ["19 แท่ง", "20 แท่ง", "21 แท่ง", "22 แท่ง", "23 แท่ง"],
-                correctAnswer: 2
-            }];
-            let questionCounter = 0; //Tracks question number
-            let selections = []; //Array containing user choices
-            const quiz = $('#quiz'); //Quiz div object
-            // Display initial question
-            displayNext();
-            // Click handler for the 'next' button
-            $('#next').on('click', function (e) {
-                e.preventDefault();
-                // Suspend click listener during fade animation
-                if (quiz.is(':animated')) {
-                    return false;
-                }
-                choose();
-                // If no user selection, progress is stopped
-                if (isNaN(selections[questionCounter])) {
-                    swal("อ๊ะ!", "กรุณาเลือกคำตอบก่อนนะครับ", "warning",{button: "ปิด", dangerMode: true});
-                } else {
-                    questionCounter++;
-                    displayNext();
-                }
-            });
-            // Click handler for the 'prev' button
-            $('#prev').on('click', function (e) {
-                e.preventDefault();
-                if (quiz.is(':animated')) {
-                    return false;
-                }
-                choose();
-                questionCounter--;
-                displayNext();
-            });
-            // Click handler for the 'Start Over' button
-            $('#start').on('click', function (e) {
-                e.preventDefault();
-                if (quiz.is(':animated')) {
-                    return false;
-                }
-                questionCounter = 0;
-                selections = [];
-                displayNext();
-                $('#start').hide();
-            });
-
-            $('#Enter').on('click', function () {
-                var numCorrect = 0;
-                for (var i = 0; i < selections.length; i++) {
-                    if (selections[i] === questions[i].correctAnswer) {
-                        numCorrect++;
-                    }
-                }
-
-
-                $.ajax({
-                    url: "insert.php?score=" + numCorrect + "&name=" + name.value + "&monitor=" +
-                        monitor.value + "&section=" + section.value,
-                    type: 'post',
-                    dataType: 'html'
-                });
-                $('#send,#quiz,#start,#start').hide();
-
-                $('#sent').show();
-
-                setTimeout(window.open("http://ath4/", "_self"), 3000);
-
-
-            });
-
-            $('.select2').select2({
-                theme: 'bootstrap'
-            });
-            // Creates and returns the div that contains the questions and
-            // the answer selections
-            function createQuestionElement(index) {
-                var qElement = $('<div>', {
-                    id: 'question'
-                });
-                var header = $('<div class="stick"><span class="label label-default">Question ' + (index + 1) +
-                    ':</span></div>');
-                qElement.append(header);
-                var header = $('<div id="q' + (index + 1) + '"></div>');
-                qElement.append(header);
-                var question = $('<h2>').append(questions[index].question);
-                qElement.append(question);
-                var radioButtons = createRadios(index);
-                qElement.append(radioButtons);
-                return qElement;
-            }
-            // Creates a list of the answer choices as radio inputs
-            function createRadios(index) {
-                var radioList = $('<div class="btn-group btn-group-md" data-toggle="buttons">');
-                var item;
-                var input = '';
-                for (var i = 0; i < questions[index].choices.length; i++) {
-                    item = $('<label class="btn btn-danger">');
-                    input = '<input type="radio" name="answer" value=' + i + ' />';
-                    input += questions[index].choices[i];
-                    item.append(input);
-                    radioList.append(item);
-                }
-                return radioList;
-            }
-            // Reads the user selection and pushes the value to an array
-            function choose() {
-                selections[questionCounter] = +$('input[name="answer"]:checked').val();
-            }
-            // Displays next requested element
-            function displayNext() {
-                quiz.fadeOut(function () {
-                    $('#question').remove();
-                    $('#send').hide();
-                    $('#sent').hide();
-                    if (questionCounter < questions.length) {
-                        var nextQuestion = createQuestionElement(questionCounter);
-                        quiz.append(nextQuestion).fadeIn();
-                        if (!(isNaN(selections[questionCounter]))) {
-                            $('input[value=' + selections[questionCounter] + ']').prop('checked', true);
-                        }
-                        // Controls display of 'prev' button
-                        if (questionCounter === 1) {
-                            $('#prev').show();
-                        } else if (questionCounter === 0) {
-                            $('#prev').hide();
-                            $('#next').show();
-                        }
-                    } else {
-                        var scoreElem = displayScore();
-                        quiz.append(scoreElem).fadeIn();
-                        $('#send').show();
-                        $('#name').focus();
-                        $('#next').hide();
-                        $('#prev').hide();
-                        $('#start').show();
-                    }
-                });
-            }
-            // Computes score and returns a paragraph element to be displayed
-            function displayScore() {
-                var score = $('<h1>', {
-                    id: 'question'
-                });
-                var numCorrect = 0;
-                for (var i = 0; i < selections.length; i++) {
-                    if (selections[i] === questions[i].correctAnswer) {
-                        numCorrect++;
-                    }
-                }
-                // score.append('You got ' + numCorrect + ' questions out of ' + questions.length + ' right!!!');
-                score.append('ขอบคุณสำหรับการทำแบบทดสอบ');
-                return score;
-            }
-        });
-    </script>
 </body>
 
 </html>
---
+++ js/app.js
@@ -0,0 +1,202 @@
+'use strict';
+const questions = [{
+    question: "เห็นวงกลมในรูปกี่วง",
+    choices: ["3 วง", "4 วง"],
+    correctAnswer: 1
+}, {
+    question: "เห็นวงกลมในรูปกี่วง",
+    choices: ["3 วง", "4 วง"],
+    correctAnswer: 1
+}, {
+    question: "เฉดสีเรียงกันสม่ำเสมอหรือไม่",
+    choices: ["ใช่", "ไม่ใช่"],
+    correctAnswer: 0
+}, {
+    question: "มีแท่งสีจำนวนกี่แท่ง เฉดสีดำ-ขาว",
+    choices: ["19 แท่ง", "20 แท่ง", "21 แท่ง", "22 แท่ง", "23 แท่ง"],
+    correctAnswer: 2
+}];
+let questionCounter = 0; //Tracks question number
+let selections = []; //Array containing user choices
+const quiz = $('#quiz'); //Quiz div object
+// Display initial question
+displayNext();
+// Click handler for the 'next' button
+$('#next').on('click', function (e) {
+    e.preventDefault();
+    // Suspend click listener during fade animation
+    if (quiz.is(':animated')) {
+        return false;
+    }
+    choose();
+    // If no user selection, progress is stopped
+    if (isNaN(selections[questionCounter])) {
+        swal("อ๊ะ!", "กรุณาเลือกคำตอบก่อนนะครับ", "warning", {
+            button: "ปิด",
+            dangerMode: true
+        });
+    } else {
+        questionCounter++;
+        displayNext();
+    }
+});
+// Click handler for the 'prev' button
+$('#prev').on('click', function (e) {
+    e.preventDefault();
+    if (quiz.is(':animated')) {
+        return false;
+    }
+    choose();
+    questionCounter--;
+    displayNext();
+});
+// Click handler for the 'Start Over' button
+$('#start').on('click', function (e) {
+    e.preventDefault();
+    if (quiz.is(':animated')) {
+        return false;
+    }
+    questionCounter = 0;
+    selections = [];
+    displayNext();
+    $('#start').hide();
+});
+
+$('#submit').on('click', function (e) {
+    e.preventDefault();
+
+    let numCorrect = 0;
+
+    for (let i = 0; i < selections.length; i++) {
+        if (selections[i] === questions[i].correctAnswer) {
+            numCorrect++;
+        }
+    }
+
+    var jqxhr = $.post('insert.php', {
+        score: numCorrect,
+        name: $('#name').val(),
+        monitor: $('#monitor').val(),
+        section: $('#section').val()
+    }, function () {
+
+        // console.log(">"+id+':'+fid+':'+fname+':'+note);
+    }).done(function () {
+
+        swal({
+                title: "All done!",
+                icon: "success",
+                // text: "บันทึกสำเร็จ",
+                text: jqxhr.responseText,
+                timer: 2000,
+                button: false
+            })
+            .then(() => {
+                window.location.href = "./";
+            });
+
+    }).fail(function (data) {
+        if (data.responseText !== '') {
+            swal({
+                title: jqxhr.responseText,
+                icon: 'warning',
+                buttons: {
+                    cancel: true
+                }
+            });
+        } else {
+            swal("Oops!", 'Oops! Something went wrong and we couldn\'t send your message', 'error');
+        }
+    });
+
+    $('#quiz,#start,#start').hide();
+
+    // $('#sent').show();
+
+    // setTimeout(window.open("http://ath4/", "_self"), 3000);
+
+
+});
+
+$('.select2').select2({
+    theme: 'bootstrap'
+});
+// Creates and returns the div that contains the questions and
+// the answer selections
+function createQuestionElement(index) {
+    var qElement = $('<div>', {
+        id: 'question'
+    });
+    var header = $('<div class="stick"><span class="label label-default">Question ' + (index + 1) +
+        ':</span></div>');
+    qElement.append(header);
+    var header = $('<div id="q' + (index + 1) + '"></div>');
+    qElement.append(header);
+    var question = $('<h2>').append(questions[index].question);
+    qElement.append(question);
+    var radioButtons = createRadios(index);
+    qElement.append(radioButtons);
+    return qElement;
+}
+// Creates a list of the answer choices as radio inputs
+function createRadios(index) {
+    var radioList = $('<div class="btn-group btn-group-md" data-toggle="buttons">');
+    var item;
+    var input = '';
+    for (var i = 0; i < questions[index].choices.length; i++) {
+        item = $('<label class="btn btn-danger">');
+        input = '<input type="radio" name="answer" value=' + i + ' />';
+        input += questions[index].choices[i];
+        item.append(input);
+        radioList.append(item);
+    }
+    return radioList;
+}
+// Reads the user selection and pushes the value to an array
+function choose() {
+    selections[questionCounter] = +$('input[name="answer"]:checked').val();
+}
+// Displays next requested element
+function displayNext() {
+    quiz.fadeOut(function () {
+        $('#question').remove();
+        $('#send').hide();
+        $('#sent').hide();
+        if (questionCounter < questions.length) {
+            var nextQuestion = createQuestionElement(questionCounter);
+            quiz.append(nextQuestion).fadeIn();
+            if (!(isNaN(selections[questionCounter]))) {
+                $('input[value=' + selections[questionCounter] + ']').prop('checked', true);
+            }
+            // Controls display of 'prev' button
+            if (questionCounter === 1) {
+                $('#prev').show();
+            } else if (questionCounter === 0) {
+                $('#prev').hide();
+                $('#next').show();
+            }
+        } else {
+            var scoreElem = displayScore();
+            quiz.append(scoreElem).fadeIn();
+            $('#send,#start').show();
+            $('#name').focus();
+            $('#next,#prev').hide();
+            getBackground(el);
+        }
+    });
+}
+// Computes score and returns a paragraph element to be displayed
+function displayScore() {
+    let score = $('<h1>', {
+        id: 'question'
+    });
+    let numCorrect = 0;
+    for (let i = 0; i < selections.length; i++) {
+        if (selections[i] === questions[i].correctAnswer) {
+            numCorrect++;
+        }
+    }
+    // score.append('You got ' + numCorrect + ' questions out of ' + questions.length + ' right!!!');
+    score.addClass('white').append('ขอบคุณสำหรับการทำแบบทดสอบ');
+    return score;
+}
